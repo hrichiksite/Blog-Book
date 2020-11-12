088d290fc60f448a34f12e3e4d1be8ba1bf7baa6
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2020-11-12 15:54:08 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: true,
 
   firebaseConfig :{
- apiKey: "",
- authDomain: "",
- databaseURL: "",
- projectId: "",
- storageBucket: "",
- messagingSenderId: "",
- appId: "",
- measurementId: ""
+    apiKey: "AIzaSyDYn545gOWlisQzncd4LWItndhA_BZYCak",
+    authDomain: "blognote-now.firebaseapp.com",
+    databaseURL: "https://blognote-now.firebaseio.com",
+    projectId: "blognote-now",
+    storageBucket: "blognote-now.appspot.com",
+    messagingSenderId: "821558527890",
+    appId: "1:821558527890:web:bcd7eca54c329cab4f234b",
+    measurementId: "G-8ET5J5CZDL"
 }
 };
